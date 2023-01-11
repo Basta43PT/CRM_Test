20d849c35d4e6d7abd79ca64bab3a03775452306
amrusiy <amrusi.yarin@gmail.com>
2023-01-11 23:21:27 +0200

    problem: when adding item to cart, why cart only contain the item that was sent instead all items

--- crm/src/category/category.jsx
+++ crm/src/category/category.jsx
@@ -2,20 +2,17 @@ import React, { useState } from "react";
 import styles from "./category.module.css";
 import { Item } from "../item";
 
-export function Category({ categoryName, items, addToCart, subFromCart }) {
+export function Category({ categoryName, items }) {
   return (
     <div className={styles.category}>
       <h1>{categoryName}</h1>
       <div className={styles.itemList}>
-        {items.map((item) => (
+        {items.map((item, index) => (
           <Item
+            // key={index}
             id={item.id}
             name={item.name}
             price={item.price}
-            count={item.count}
-            // onClick={onClick}
-            addToCart={addToCart}
-            subFromCart={subFromCart}
           />
         ))}
       </div>
